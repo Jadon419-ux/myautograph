02943f1681cb5be5738b7201938502597c571e93
kayrex-dev <nendelkaye@gmail.com>
2026-08-26 22:47:10 +0100

    Add Google Play download CTA to landing page

--- frontend/src/brand.js
+++ frontend/src/brand.js
@@ -5,5 +5,6 @@ export const BRAND = {
   website: "www.myautographma.com",
   phone: "+234 901 967 2650",
   instagram: "https://www.instagram.com/myautograp_app?igsh=MXdoaXo4YWx3Z2E5",
+  playStoreUrl: "https://play.google.com/store/apps/details?id=com.myautograph.auto_graph_app",
   registrationNumber: "8730557",
 };
